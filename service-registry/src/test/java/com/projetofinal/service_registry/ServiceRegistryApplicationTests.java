ba214520185af86382fd5c03c808d0a26560e9ec
Williams Alves Dantas <williamsad9@gmail.com>
2024-10-20 08:58:17 -0300

    criação do projeto do service-registry

---
+++ service-registry/src/test/java/com/projetofinal/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetofinal.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
