83faeb10f6889becb1dce495d66a08225eb0d845
Williams Alves Dantas <williamsad9@gmail.com>
2024-10-20 22:15:33 -0300

    api.pedidos - integração com api.produtos

--- api.pedidos/src/main/java/com/projetofinal/api/pedidos/external/ProdutoClient.java
+++ api.pedidos/src/main/java/com/projetofinal/api/pedidos/external/ProdutoClient.java
@@ -1,15 +1,15 @@
 package com.projetofinal.api.pedidos.external;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.ResponseEntity;
 
-@FeignClient(name = "produto-service", url = "http://localhost:8081/api/produtos")  // Ajuste a URL conforme necessário
+@FeignClient(name = "produto-service" , url = "http://localhost:8082/produtos")
 public interface ProdutoClient {
 
-    @PostMapping("/{id}/reservar")
+    @PostMapping(value = "/{id}/reservar" , consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<String> reservarProduto(@PathVariable("id") Long id);
 }
 
